37aa95e65958926d23c753fa9b7ddf3a41e6b7db
GIRISH <access2gjadav@gmail.com>
2026-03-03 09:03:45 -0500

    Fix API export for Vercel

--- api/index.ts
+++ api/index.ts
@@ -7,15 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-await registerRoutes(app);
+// wrap async setup properly
+async function init() {
+  await registerRoutes(app);
 
-// Serve frontend
-const distPath = path.join(process.cwd(), "dist");
+  const distPath = path.join(process.cwd(), "dist");
 
-app.use(express.static(distPath));
+  app.use(express.static(distPath));
 
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(distPath, "index.html"));
-});
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
+
+init();
 
 export default app;
